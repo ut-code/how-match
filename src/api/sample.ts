61654b48e27a33cd62ca7dea74ac52a231d2ecfa
aster <137767097+aster-void@users.noreply.github.com>
2025-01-26 19:01:51 +0900

    rename worker -> service, call service type from sveltekit

---
+++ src/api/sample.ts
@@ -0,0 +1,10 @@
+import { hc } from "hono/client";
+import type { App } from "service/src/app";
+
+export const client = hc<App>("http://localhost:3000");
+
+client.$get({
+  json: {
+    id: "hi",
+  },
+});
