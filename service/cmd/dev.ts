8bb9803badb6f4fb67aa37e82e6fceb9233403ee
aster <137767097+aster-void@users.noreply.github.com>
2025-01-27 02:54:24 +0900

    fix cmd/dev

--- service/cmd/dev.ts
+++ service/cmd/dev.ts
@@ -1,4 +1,7 @@
-import { app } from "..";
+import { init } from "..";
+import { db } from "../db/local.ts";
+
+const app = init(db);
 
 Bun.serve({
   fetch: app.fetch,
---
+++ service/db/local.ts
@@ -0,0 +1,2 @@
+import { drizzle } from "drizzle-orm/libsql";
+export const db = drizzle("./local.db");
