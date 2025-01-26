eaacbbb6fcd9cf0d7c2d4951b30743083bba361c
aster <137767097+aster-void@users.noreply.github.com>
2025-01-27 02:34:30 +0900

    db: alter schema

--- service/db/schema.ts
+++ service/db/schema.ts
@@ -1,6 +1,7 @@
-import { integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
 export const user = sqliteTable("user", {
   id: integer("id").primaryKey(),
   age: integer("age"),
+  name: text("name"),
 });
