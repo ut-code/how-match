82b00a61b0c9e8d08c0a80fdc2d704baef02baf4
aster <137767097+aster-void@users.noreply.github.com>
2025-01-28 00:41:13 +0900

    web: setup tailwind (v4) + daisyui (v5-beta), and config biome to accept svelte files

--- service/db/schema.ts
+++ service/db/schema.ts
@@ -1,4 +1,4 @@
-import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const user = sqliteTable("user", {
   id: integer("id").primaryKey(),
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
 </script>
 
-<h1>Hello!</h1>
+<h1 class="text-2xl text-red-500">Hello!</h1>
+<fieldset class="fieldset">
+  <legend class="fieldset-legend">Email</legend>
+  <label class="input validator">
+    <input type="name" />
+    <span class="label">.github.io</span>
+  </label>
+  <p class="fieldset-label">You can edit page title later on from settings</p>
+</fieldset>
