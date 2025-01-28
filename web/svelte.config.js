e40c2107d5cfddbc7cbd0e1cf842ee2e4fa559c3
aster <137767097+aster-void@users.noreply.github.com>
2025-01-28 12:20:51 +0900

    web: rename adapter -> staticAdapter

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-import adapter from "@sveltejs/adapter-static";
+import staticAdapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
 
@@ -13,7 +13,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({
+    adapter: staticAdapter({
       strict: true,
       pages: "dist",
       assets: "dist",
