c8ea572f730af54196180ec9a00ef76274d515f9
aster <137767097+aster-void@users.noreply.github.com>
2025-01-29 20:11:43 +0900

    service/db: make the file names clearer

--- service/cmd/dev.ts
+++ service/cmd/dev.ts
@@ -1,6 +1,6 @@
 import Bun from "bun";
 
-import { db } from "../db/local.ts";
+import { db } from "../db/client.ts";
 import { init } from "../index.ts";
 
 const app = init(db);
--- service/db/local.ts
+++
@@ -1,2 +0,0 @@
-import { drizzle } from "drizzle-orm/libsql";
-export const db = drizzle("./local.db");
