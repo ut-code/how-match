67d333a86ee91c5ca58f0130d7b056b740481c86
naka-12 <104970808+naka-12@users.noreply.github.com>
2025-02-13 18:39:03 +0900

    fix: adapter-cloudflare を追加

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,4 +1,5 @@
 // @ts-check
+import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
 
@@ -9,6 +10,19 @@ const config = {
   preprocess: [vitePreprocess(), mdsvex()],
 
   kit: {
+    adapter: adapter({
+      // See below for an explanation of these options
+      routes: {
+        include: ["/*"],
+        exclude: ["<all>"],
+      },
+      // platformProxy: {
+      // 	configPath: 'wrangler.toml',
+      // 	environment: undefined,
+      // 	experimentalJsonConfig: false,
+      // 	persist: false
+      // }
+    }),
     files: {
       // https://stackoverflow.com/questions/71789244/where-to-put-images-with-sveltekit
       assets: "./static",
