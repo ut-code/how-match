3845f183206c39120971c6dbb5d74a58470083ba
naka-12 <104970808+naka-12@users.noreply.github.com>
2025-02-13 18:34:04 +0900

    fix: dynamic routes のために SSG 用アダプタを外した

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,4 @@
 // @ts-check
-import staticAdapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
 
@@ -10,14 +9,6 @@ const config = {
   preprocess: [vitePreprocess(), mdsvex()],
 
   kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: staticAdapter({
-      strict: true,
-      pages: "dist",
-      assets: "dist",
-    }),
     files: {
       // https://stackoverflow.com/questions/71789244/where-to-put-images-with-sveltekit
       assets: "./static",
