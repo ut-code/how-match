fa0ee138941b6a6b5612f814d47dff3288426af7
naka-12 <104970808+naka-12@users.noreply.github.com>
2025-02-13 17:53:36 +0900

    feat: スタイリングのサンプル

--- web/src/routes/new/+page.svelte
+++ web/src/routes/new/+page.svelte
@@ -1,3 +1,17 @@
+<script>
+  import Header from "~/components/header.svelte";
+</script>
 <div>
-  <h1 class="text-primary">プロジェクトの作成</h1>
+  <Header title="プロジェクトの作成" />
+  <div class="mt-12 h-full bg-base-100 p-6 flex flex-col gap-4">
+    <div class="rounded-lg bg-white p-6 flex flex-col gap-2">
+      <p>sample</p>
+      <input type="text" class="input bg-white" placeholder="sample" />
+    </div>
+    <div class="flex justify-end">
+      <button type="button" class="btn btn-primary">
+        作成
+      </button>
+    </div>
+  </div>
 </div>
