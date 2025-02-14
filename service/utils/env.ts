fe41f69379220d03fa4f67cc87467cacb0096b7c
aster <137767097+aster-void@users.noreply.github.com>
2025-02-14 16:08:40 +0900

    fix: cannot read propety of undefined (reading CORS_ALLOW_ORIGINS

--- service/utils/env.ts
+++ service/utils/env.ts
@@ -3,7 +3,7 @@ import { env as _env } from "hono/adapter";
 import { panic } from "./panic.ts";
 
 export function env(ctx: Context, name: string) {
-  return _env(ctx)[name] ??
+  return _env(ctx)?.[name] ??
     panic(`env var "${name}" not found in environment!`);
 }
 
