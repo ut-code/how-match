9c1cc6647dded7ea8a4231f801bf6d7795222f93
aster <137767097+aster-void@users.noreply.github.com>
2025-02-14 16:45:29 +0900

    test

--- web/src/routes/api/[...paths]/+server.ts
+++ web/src/routes/api/[...paths]/+server.ts
@@ -2,7 +2,8 @@ import type { RequestEvent } from "@sveltejs/kit";
 import app from "service/src";
 
 async function hook(event: RequestEvent) {
-  return await app.fetch(event.request, event.platform);
+  // @ts-ignore: TypeScript can't recognize app.d.ts?
+  return await app.fetch(event.request, event.platform.env);
 }
 
 export const GET = hook;
