fff3e4d60ea0c0496ba3b67b9205fc5912a0d16a
furuSchool <147578340+furuSchool@users.noreply.github.com>
2025-02-14 14:08:09 +0900

    endpointの実装

--- service/db/schema.ts
+++ service/db/schema.ts
@@ -1,44 +1,46 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const participant = sqliteTable("Participant", {
-  id: integer("id").notNull().primaryKey(),
-  account_id: integer("account_id").references(() => account.id).notNull(),
-  project_id: integer("project_id").references(() => project.id).notNull(),
+  id: text("id").notNull().primaryKey(),
+  account_id: text("account_id").references(() => account.id).notNull(),
+  project_id: text("project_id").references(() => project.id).notNull(),
   is_admin: integer("is_admin").notNull(),
 });
 
 export const account = sqliteTable("Account", {
-  id: integer().notNull().primaryKey(),
+  id: text().notNull().primaryKey(),
   name: text().notNull(),
 });
 
 export const project = sqliteTable("Project", {
-  id: integer().notNull().primaryKey(),
+  id: text().notNull().primaryKey(),
   name: text().notNull(),
+  description: text(),
+  closed_at: text(),
 });
 
 export const role = sqliteTable("Role", {
-  id: integer().notNull().primaryKey(),
+  id: text().notNull().primaryKey(),
   min: integer().notNull(),
   max: integer().notNull(),
-  project_id: integer("project_id").references(() => project.id).notNull(),
+  project_id: text("project_id").references(() => project.id).notNull(),
 });
 
 export const rating = sqliteTable("Rating", {
-  id: integer().notNull().primaryKey(),
-  participant_id: integer("participant_id").references(() => participant.id)
+  id: text().notNull().primaryKey(),
+  participant_id: text("participant_id").references(() => participant.id)
     .notNull(),
-  role_id: integer("role_id").references(() => role.id).notNull(),
+  role_id: text("role_id").references(() => role.id).notNull(),
   score: integer().notNull(),
-  project_id: integer("project_id").references(() => project.id).notNull(),
+  project_id: text("project_id").references(() => project.id).notNull(),
 });
 
 export const match = sqliteTable("Match", {
-  id: integer().notNull().primaryKey(),
-  role_id: integer("role_id").references(() => role.id).notNull(),
-  participant_id: integer("participant_id").references(() => participant.id)
+  id: text().notNull().primaryKey(),
+  role_id: text("role_id").references(() => role.id).notNull(),
+  participant_id: text("participant_id").references(() => participant.id)
     .notNull(),
-  project_id: integer("project_id").references(() => project.id).notNull(),
+  project_id: text("project_id").references(() => project.id).notNull(),
 });
 
 export type InsertParticipant = typeof participant.$inferInsert;
--- service/src/index.ts
+++ service/src/index.ts
@@ -1,10 +1,7 @@
-import { zValidator } from "@hono/zod-validator";
 import { type Context, Hono } from "hono";
 import { cors } from "hono/cors";
-import { account, match, participant, rating, role } from "../db/schema.ts";
-import { db } from "../db/client.ts";
 import { env } from "../utils/env.ts";
-import { z } from "zod";
+import { projects } from "./routes/projects.ts";
 
 const corsOptions = (c: Context) => ({
   origin: env(c, "CORS_ALLOW_ORIGINS").split(","),
@@ -19,143 +16,7 @@ const app = new Hono()
     );
   })
   .get("/", (c) => c.text("Hello from Hono!"))
-  .post(
-    "/",
-    zValidator(
-      "json",
-      z.object({
-        name: z.string(),
-      }),
-    ),
-    (c) => {
-      const name = c.req.valid("json").name;
-      return c.text(`Hello ${name}!`);
-    },
-  )
-  .get("/accounts", async (c) => {
-    return c.json(await db(c).select().from(account).execute());
-  })
-  // .get("/participants", async (c) => {
-  //   return c.json(await db(c).select().from(participant).execute());
-  // })
-  .post(
-    "/participants",
-    zValidator(
-      "json",
-      z.object({
-        account_id: z.coerce.number(),
-        project_id: z.coerce.number(),
-        is_admin: z.coerce.boolean(),
-      }),
-    ),
-    async (c) => {
-      const body = c.req.valid("json");
-      const resp = await db(c).insert(participant).values([
-        {
-          id: 0, // uuid あとで
-          account_id: body.account_id,
-          project_id: body.project_id,
-          is_admin: Number(body.is_admin),
-        },
-      ]);
-      console.log("added", resp);
-      return c.json(resp);
-    },
-  )
-  .post(
-    "/accounts",
-    zValidator(
-      "json",
-      z.object({
-        name: z.string(),
-      }),
-    ),
-    async (c) => {
-      const body = c.req.valid("json");
-      const resp = await db(c).insert(account).values([
-        {
-          id: 0, // uuid あとで
-          name: body.name,
-        },
-      ]);
-      console.log("added", resp);
-      return c.json(resp);
-    },
-  )
-  .post(
-    "/role",
-    zValidator(
-      "json",
-      z.object({
-        min: z.coerce.number(),
-        max: z.coerce.number(),
-        project_id: z.coerce.number(),
-      }),
-    ),
-    async (c) => {
-      const body = c.req.valid("json");
-      const resp = await db(c).insert(role).values([
-        {
-          id: 0, // uuid あとで
-          min: body.min,
-          max: body.max,
-          project_id: body.project_id,
-        },
-      ]);
-      console.log("added", resp);
-      return c.json(resp);
-    },
-  )
-  .post(
-    "/rating",
-    zValidator(
-      "json",
-      z.object({
-        participant_id: z.coerce.number(),
-        role_id: z.coerce.number(),
-        score: z.coerce.number(),
-        project_id: z.coerce.number(),
-      }),
-    ),
-    async (c) => {
-      const body = c.req.valid("json");
-      const resp = await db(c).insert(rating).values([
-        {
-          id: 0, // uuid あとで
-          participant_id: body.participant_id,
-          role_id: body.role_id,
-          score: body.score,
-          project_id: body.project_id,
-        },
-      ]);
-      console.log("added", resp);
-      return c.json(resp);
-    },
-  )
-  .post(
-    "/match",
-    zValidator(
-      "json",
-      z.object({
-        role_id: z.coerce.number(),
-        participant_id: z.coerce.number(),
-        project_id: z.coerce.number(),
-      }),
-    ),
-    async (c) => {
-      const body = c.req.valid("json");
-      const resp = await db(c).insert(match).values([
-        {
-          id: 0, // uuid あとで
-          role_id: body.role_id,
-          participant_id: body.participant_id,
-          project_id: body.project_id,
-        },
-      ]);
-      console.log("added", resp);
-      return c.json(resp);
-    },
-  );
+  .route("/projects", projects);
 
 console.log("running");
 
