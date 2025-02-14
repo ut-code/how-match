e0ac5bd5fef59f25c3d433ec3ddb06db76c76f0e
aster <137767097+aster-void@users.noreply.github.com>
2025-02-14 16:49:28 +0900

    FIX: Hono can't find cloudflare bindings

--- web/src/routes/api/[...paths]/+server.ts
+++ web/src/routes/api/[...paths]/+server.ts
@@ -1,8 +1,10 @@
 import type { RequestEvent } from "@sveltejs/kit";
 import app from "service/src";
+import { panic } from "service/utils/panic";
 
 async function hook(event: RequestEvent) {
-  return await app.fetch(event.request, event.platform);
+  // @ts-ignore: TypeScript can't recognize platform type? FIXME: make it recognize App.env from src/app.d.ts
+  return await app.fetch(event.request, (event.platform ?? panic("platform not found")).env);
 }
 
 export const GET = hook;
