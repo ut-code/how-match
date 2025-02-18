f7b4f18806eb00498e00cd929a93459bf82c8936
aster <137767097+aster-void@users.noreply.github.com>
2025-02-18 12:29:26 +0900

    wip

--- web/src/routes/[projectId]/config/+page.svelte
+++ web/src/routes/[projectId]/config/+page.svelte
@@ -1,9 +1,35 @@
 <script lang="ts">
-  import type { PageProps } from "./$types.ts";
-  let { data }: PageProps = $props();
+  import chain from "~/icons/Chain.svg";
+  const { data } = $props();
+
+  class Readonly<T> {
+    #val: T;
+    constructor(val: T) {
+      this.#val = val;
+    }
+    get val() {
+      return this.#val;
+    }
+  }
+
+  // TODO: fix this
+  const domain = "localhost:5173";
+  const link = $derived(
+    new Readonly(`https://${domain}/${data.projectId}/submit`),
+  );
 </script>
 
 <div>
   <h1>設定</h1>
   <p>Project ID: {data.projectId}</p>
 </div>
+
+<a class="btn" href="./submit">提出する</a>
+
+<label class="input input-bordered">
+  <img alt="" src={chain} />
+  提出用リンク
+  <!-- TODO: output 専用だけど input タグでよい？ -->
+  <input type="url" class="input input-bordered" bind:value={link.val} />
+  <button>copy</button>
+</label>
