c435a160336d015073a6b99f2a7390a8ac95477d
aster <137767097+aster-void@users.noreply.github.com>
2025-02-18 12:35:38 +0900

    style: make sqlite table names follow sql convention

--- service/drizzle.config.ts
+++ service/drizzle.config.ts
@@ -1,7 +1,6 @@
 import { process_env } from "./utils/env.ts";
 import { defineConfig } from "drizzle-kit";
 
-// biome-ignore lint/style/noDefaultExport: it's used by drizzle-kit and I can't change the behaviour
 export default defineConfig({
   schema: "./db/schema.ts",
 
--- service/src/routes/sample.ts
+++ service/src/routes/sample.ts
@@ -1,11 +1,11 @@
 import { zValidator } from "@hono/zod-validator";
-import { account } from "../../db/schema.ts";
+import { accounts } from "../../db/schema.ts";
 import { Hono } from "hono";
 import type { HonoOptions } from "../types.ts";
 import { z } from "zod";
 import { db } from "../../db/client.ts";
 
-export const accounts = new Hono<HonoOptions>()
+const route = new Hono<HonoOptions>()
   .post(
     "/",
     zValidator(
@@ -16,7 +16,7 @@ export const accounts = new Hono<HonoOptions>()
     ),
     async (c) => {
       const body = c.req.valid("json");
-      const resp = await db(c).insert(account).values([
+      const resp = await db(c).insert(accounts).values([
         {
           id: Math.random().toString(), // uuid あとで
           name: body.name,
@@ -27,6 +27,7 @@ export const accounts = new Hono<HonoOptions>()
     },
   )
   .get("/", async (c) => {
-    const resp = await db(c).select().from(account).execute();
+    const resp = await db(c).select().from(accounts);
     return c.json(resp);
   });
+export default route;
