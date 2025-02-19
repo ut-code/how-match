10d10ad181c313a16f14ab4a8ac4a86f3b648879
aster <137767097+aster-void@users.noreply.github.com>
2025-02-19 15:18:51 +0900

    fix: baseUrl that gets logged is always empty

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -3,7 +3,7 @@ import type { App } from "service/src";
 
 // during development, use isolated server for better hot reload
 const baseUrl = import.meta.env.DEV ? "http://localhost:3000/" : "/";
-console.log(`[log] hono/client: baseUrl = ${import.meta.env.baseUrl}`);
+console.log(`[log] hono/client: baseUrl = ${baseUrl}`);
 
 export const client = hc<App>(baseUrl, {
   init: {
