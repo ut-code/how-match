06242f60f1f1e21aae41c94bda46899e8f0ce200
aster <137767097+aster-void@users.noreply.github.com>
2025-02-21 09:37:56 +0900

    wip

--- service/config/d1.drizzle.config.ts
+++ service/config/d1.drizzle.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "drizzle-kit";
+import { process_env } from "../utils/env.ts";
 
 export default defineConfig({
   verbose: true,
-  schema: "../db/schema.ts",
+  schema: "./db/schema.ts",
 
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    accountId: "",
-    databaseId: "",
-    token: "",
+    accountId: process_env("CLOUDFLARE_ACCOUNT_ID"),
+    databaseId: process_env("CLOUDFLARE_DATABASE_ID"),
+    token: process_env("CLOUDFLARE_D1_TOKEN"),
   },
 });
--- service/config/drizzle.config.ts
+++ service/config/drizzle.config.ts
@@ -2,7 +2,7 @@ import { process_env } from "../utils/env.ts";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "../db/schema.ts",
+  schema: "./db/schema.ts",
 
   dbCredentials: {
     url: process_env("DATABASE_URL"),
