fb3f4902e668de424531c0804dcdff0a7c18b0eb
aster <137767097+aster-void@users.noreply.github.com>
2025-02-22 16:16:19 +0900

    another test

--- web/src/routes/created/+page.ts
+++ web/src/routes/created/+page.ts
@@ -3,15 +3,11 @@ import { client } from "~/api/client";
 import type { PageLoad } from "./$types.ts";
 import { redirect } from "@sveltejs/kit";
 
-async function validateProjectId(projectId: string) {
-  const response = await client.projects[":projectId"].$get({ param: { projectId } });
-  return response.ok ? projectId : null;
-}
-
 export const load: PageLoad = async ({ url }) => {
   const projectId = url.searchParams.get("projectId");
   if (!projectId) redirect(303, "/");
-  const validProjectId = await validateProjectId(projectId);
-  if (!validProjectId) redirect(303, "/");
-  return { projectId: validProjectId };
+  if ((await client.projects[":projectId"].$get({ param: { projectId } })).ok) {
+    return { projectId };
+  }
+  redirect(303, "/");
 };
