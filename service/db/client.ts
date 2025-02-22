8aa0e66fb7267508942037eef16255f3f717a5be
aster <137767097+aster-void@users.noreply.github.com>
2025-02-22 21:33:56 +0900

    revert HEAD~2: use libsql again, because their types are not compat

--- service/db/client.ts
+++ service/db/client.ts
@@ -1,5 +1,5 @@
-import { drizzle as bun_sqlite } from "drizzle-orm/libsql";
-import { drizzle as d1 } from "drizzle-orm/d1";
+import { drizzle as libsql } from "drizzle-orm/libsql";
+import { drizzle as d1, type DrizzleD1Database } from "drizzle-orm/d1";
 import type { HonoOptions } from "../types.ts";
 import type { Context } from "hono";
 
@@ -13,5 +13,6 @@ export const db = (c: Context<HonoOptions>) => {
     throw new Error("ERROR: c.env.DB not found");
   }
 
-  return bun_sqlite("file:../local.db");
+  const db: DrizzleD1Database = libsql("file:../local.db");
+  return db;
 };
