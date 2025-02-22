a4ebd19da764d90ca0bb39d734fc230f2bea2dca
aster <137767097+aster-void@users.noreply.github.com>
2025-02-22 19:34:12 +0900

    feat: home button

--- web/src/components/header.svelte
+++ web/src/components/header.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <header class="fixed top-0 left-0 w-full z-30 border-b border-primary bg-white">
-  <div class="h-12 px-6 flex items-center justify-between">
+  <div class="h-12 px-6 flex items-start align-middle">
+    <a class="btn btn-primary" href="/"> アイコン </a>
     <span class="text-xl">
       {title}
     </span>
