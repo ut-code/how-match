8cd9f96610e3789ad131d2753406fac738c84bcf
aster <137767097+aster-void@users.noreply.github.com>
2025-02-22 19:23:37 +0900

    style: use function instead of const () => {}

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -2,17 +2,17 @@ import { browser } from "$app/environment";
 import { hc } from "hono/client";
 import type { App } from "service";
 
-export type Client = ReturnType<typeof hc<App>>["api"];
-export type Fetch = typeof fetch;
-
 // during development, use isolated server for better hot reload
 const baseUrl = import.meta.env.DEV ? "http://localhost:3000/" : "/";
 console.log(`[log] hono/client: baseUrl = ${baseUrl}`);
 
-export const createClient = ({ fetch }: { fetch: Fetch }) =>
-  hc<App>(baseUrl, {
+export type Client = ReturnType<typeof hc<App>>["api"];
+export type Fetch = typeof fetch;
+export function createClient({ fetch }: { fetch: Fetch }): Client {
+  return hc<App>(baseUrl, {
     fetch,
     init: {
       credentials: browser ? "include" : undefined,
     },
   }).api;
+}
