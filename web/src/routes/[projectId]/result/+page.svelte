101bd9e481ea1ff20823d24952bd5d3856b38bc3
Tsurubara-UTcode <168950066+Tsurubara-UTcode@users.noreply.github.com>
2025-02-22 10:54:28 +0900

    result pageのほうも修正

--- web/src/routes/[projectId]/result/+page.svelte
+++ web/src/routes/[projectId]/result/+page.svelte
@@ -17,6 +17,8 @@
     participant_id: string;
     role_name: string;
     account_name: string;
+    project_name: string;
+    project_desc: string;
   }[] = [];
 
   onMount(async () => {
@@ -49,11 +51,11 @@
 </style>
 
 <div>
-  <Header title={data.projectId} />
+  <Header title={matchList.length > 0 ? matchList[0].project_name : "プロジェクト名未取得"} />
   <div class="mt-12 h-full bg-base-100 p-6 flex flex-col gap-4">
     <div class="rounded-lg bg-white p-6 flex flex-col gap-2">
-      <p>{data.projectId}</p>
-      <h1>以下の役職を募集しました。</h1>
+      <p>{matchList.length > 0 ? matchList[0].project_name : "プロジェクト名未取得"}</p>
+      <h1>{matchList.length > 0 ? matchList[0].project_desc : "プロジェクト説明未取得"}</h1>
       {#each matchList as match}
         <div>
           <h2>{match.role_name}</h2>
