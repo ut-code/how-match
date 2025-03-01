d760d3162eb7be47073b53920af5670cb46d0010
naka-12 <104970808+naka-12@users.noreply.github.com>
2025-03-01 22:46:58 +0900

    ui: renew header

--- web/src/components/header.svelte
+++ web/src/components/header.svelte
@@ -2,10 +2,10 @@
   let { title } = $props();
 </script>
 
-<header
-  class="sticky top-0 left-0 w-full z-30 border-b border-primary bg-white"
->
-  <div class="h-12 px-6 gap-8 flex items-center align-middle">
+<header class="sticky top-0 left-0 w-full z-30 px-6 pt-6">
+  <div
+    class="h-14 px-6 gap-8 flex items-center align-middle rounded-full bg-white/60 backdrop-blur-md border border-gray-200"
+  >
     <a class="font-bold text-xl cursor-pointer" href="/">how-match</a>
     <span class="text-xl">
       {title}
