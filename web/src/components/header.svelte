aeae4c221389458aa1db4dee48ad583bd4cec50a
aster <137767097+aster-void@users.noreply.github.com>
2025-03-06 01:28:39 +0900

    fix all issues

--- web/src/components/header.svelte
+++ web/src/components/header.svelte
@@ -30,7 +30,7 @@
 
 <header class="sticky top-0 left-0 w-full z-30 px-6 pt-6 flex-row">
   <div
-    class="h-14 px-6 gap-8 flex items-center align-middle rounded-xl bg-white/60 backdrop-blur-md border border-gray-200"
+    class="h-14 px-6 gap-8 flex items-center align-middle rounded-xl bg-white/60 dark:bg-gray-500/60 backdrop-blur-md border border-gray-200 dark:border-gray-400"
   >
     <a class="font-bold text-xl cursor-pointer" href="/">how-match</a>
     <span class="text-xl">
