16423b265fb30950d240ebce87dbab5cbb558681
aster <137767097+aster-void@users.noreply.github.com>
2025-06-29 11:31:38 +0900

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -4,21 +4,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    bunnix.url = "github:aster-void/bunnix";
-    bunnix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
-    bunnix,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        devShells.default = pkgs.callPackage ./shell.nix {inherit bunnix;};
+        devShells.default = pkgs.callPackage ./shell.nix {};
       }
     );
 }
--- shell.nix
+++ shell.nix
@@ -1,31 +1,19 @@
-{
-  pkgs ? import <nixpkgs> {},
-  system,
-  bunnix,
-}: let
-  bun_latest = bunnix.packages.${system}.latest;
-in
-  pkgs.mkShell {
-    name = "How Match development";
-    packages = with pkgs; [
-      # general
-      gitleaks # secret leak detection
-      just # task runner
-      litecli # sqlite CLI client with advanced features
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  name = "How Match development";
+  packages = with pkgs; [
+    # general
+    gitleaks # secret leak detection
+    just # task runner
+    litecli # sqlite CLI client with advanced features
 
-      # JS
-      bun_latest # runner / tester
-      nodejs-slim # required by drizzle?
+    # JS
+    bun # runner / tester
+    nodejs-slim # required by drizzle?
 
-      # wrangler # can't build for now
+    wrangler # can't build for now
 
-      # Database
-      turso-cli # Turso database CLI
-
-      # Nix
-      alejandra # formatter
-      nil # langserver
-      statix # nix linter
-      deadnix # code analyzer (detects dead code)
-    ];
-  }
+    # Database
+    turso-cli # Turso database CLI
+  ];
+}
